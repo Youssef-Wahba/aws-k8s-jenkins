154aa22f17c79f7a2b25db4cd26f616dcb39d587
Ahmed Elsayed <ahmed33elsayed22@gmail.com>
2024-10-03 03:15:30 +0300

    error in printing output

--- terraform/ec2_instances.tf
+++ terraform/ec2_instances.tf
@@ -4,9 +4,12 @@ resource "aws_instance" "k3s_instance" {
   instance_type          = local.instance_type
   subnet_id              = aws_subnet.public_subnet.id
   key_name               = local.key_name
-  vpc_security_group_ids = [aws_security_group.ec2_security_group.id] # Use VPC security group IDs
+  vpc_security_group_ids = [aws_security_group.ec2_security_group.id]
 
   iam_instance_profile = aws_iam_instance_profile.ec2_profile.name
+
+  # Enable public IP address
+  associate_public_ip_address = true
 
   tags = {
     Name = "k3s-instance"
@@ -19,9 +22,12 @@ resource "aws_instance" "jenkins_docker_instance" {
   instance_type          = local.instance_type
   subnet_id              = aws_subnet.public_subnet.id
   key_name               = local.key_name
-  vpc_security_group_ids = [aws_security_group.ec2_security_group.id] # Use VPC security group IDs
+  vpc_security_group_ids = [aws_security_group.ec2_security_group.id]
 
   iam_instance_profile = aws_iam_instance_profile.ec2_profile.name
+
+  # Enable public IP address
+  associate_public_ip_address = true
 
   tags = {
     Name = "jenkins-docker-instance"
