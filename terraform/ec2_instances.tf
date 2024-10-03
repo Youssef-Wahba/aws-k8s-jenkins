0c908643fc18918b855c274984abf3b65e23131d
Youssef Wahba <y.husseinwahba@gmail.com>
2024-10-03 22:08:32 +0300

    ansible and terraform k3s instance user data

--- terraform/ec2_instances.tf
+++ terraform/ec2_instances.tf
@@ -10,7 +10,28 @@ resource "aws_instance" "k3s_instance" {
 
   # Enable public IP address
   associate_public_ip_address = true
+  user_data = <<-EOF
+    # Install k3s
+    curl -sfL https://get.k3s.io | sh -s - --write-kubeconfig-mode 644
 
+    # Enable k3s service to start automatically
+    systemctl enable k3s
+    systemctl start k3s
+
+    # Install Helm
+    curl https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3 | bash
+
+    # Add the NGINX ingress Helm repo and update
+    helm repo add ingress-nginx https://kubernetes.github.io/ingress-nginx
+    helm repo update
+
+    # Install NGINX ingress controller
+    helm install nginx-ingress ingress-nginx/ingress-nginx
+
+    # Expose the kubeconfig file to allow root access
+    export KUBECONFIG=/etc/rancher/k3s/k3s.yaml
+    chmod 644 /etc/rancher/k3s/k3s.yaml
+  EOF
   tags = {
     Name = "k3s-instance"
   }
@@ -21,7 +42,7 @@ resource "aws_instance" "jenkins_docker_instance" {
   ami                    = local.ami
   instance_type          = local.instance_type
   subnet_id              = aws_subnet.public_subnet.id
-  key_name                 = aws_key_pair.ec2_key_pair.key_name
+  key_name               = aws_key_pair.ec2_key_pair.key_name
   vpc_security_group_ids = [aws_security_group.ec2_security_group.id]
 
   iam_instance_profile = aws_iam_instance_profile.ec2_profile.name
