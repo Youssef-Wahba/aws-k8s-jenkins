311ce1997b637007a58763bad440326add369769
Youssef Wahba <y.husseinwahba@gmail.com>
2024-10-03 23:35:22 +0300

    user data in k3s_instance

--- terraform/ec2_instances.tf
+++ terraform/ec2_instances.tf
@@ -11,26 +11,29 @@ resource "aws_instance" "k3s_instance" {
   # Enable public IP address
   associate_public_ip_address = true
   user_data = <<-EOF
-    # Install k3s
+    #!/bin/bash
+    set -e
+
+    echo "Installing k3s..."
     curl -sfL https://get.k3s.io | sh -s - --write-kubeconfig-mode 644
 
-    # Enable k3s service to start automatically
+    echo "Starting k3s service..."
     systemctl enable k3s
     systemctl start k3s
 
-    # Install Helm
+    echo "Setting kubeconfig permissions..."
+    chmod 600 /etc/rancher/k3s/k3s.yaml
+    export KUBECONFIG=/etc/rancher/k3s/k3s.yaml
+
+    echo "Installing Helm..."
     curl https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3 | bash
 
-    # Add the NGINX ingress Helm repo and update
+    echo "Adding NGINX ingress Helm repo..."
     helm repo add ingress-nginx https://kubernetes.github.io/ingress-nginx
     helm repo update
 
-    # Install NGINX ingress controller
+    echo "Installing NGINX ingress controller..."
     helm install nginx-ingress ingress-nginx/ingress-nginx
-
-    # Expose the kubeconfig file to allow root access
-    export KUBECONFIG=/etc/rancher/k3s/k3s.yaml
-    chmod 644 /etc/rancher/k3s/k3s.yaml
   EOF
   tags = {
     Name = "k3s-instance"
@@ -44,9 +47,7 @@ resource "aws_instance" "jenkins_docker_instance" {
   subnet_id              = aws_subnet.public_subnet.id
   key_name               = aws_key_pair.generated_key_pair.key_name
   vpc_security_group_ids = [aws_security_group.ec2_security_group.id]
-
   iam_instance_profile = aws_iam_instance_profile.ec2_profile.name
-
   # Enable public IP address
   associate_public_ip_address = true
 
